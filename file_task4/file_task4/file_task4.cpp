c04d5842c959ed98af10917facc07429a7dee6b9
garoupol@gmail.com <garoupol@gmail.com>
2015-05-03 12:36:15 +0300

    инициализация

--- file_task4/file_task4/file_task4.cpp
+++ file_task4/file_task4/file_task4.cpp
@@ -1,10 +1,21 @@
 // file_task4.cpp: определяет точку входа для консольного приложения.
 //
 #include "stdafx.h"
+#include "MyFile.h"
+#include <vector>
+#include <string>
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	CMyFile file;
+	string fileInputName = argv[1];
+	string fileOutputName = argv[2];
+
+	
+		
+	vector<string> strings;
 	return 0;
 }
 
